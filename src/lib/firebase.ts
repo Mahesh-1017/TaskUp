db3348e5c9072efcb4c5f8b14af8cfb0ecce81e5
Mahesh Vinnakota <maheshv1017@gmail.com>
2025-08-01 11:33:41 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  projectId: 'numeroconnect-637lh',
-  appId: '1:818339710146:web:01271839b424e1d7d9675b',
-  storageBucket: 'numeroconnect-637lh.firebasestorage.app',
-  apiKey: 'AIzaSyDegTkPCfQC6nYziFLncbfnM7r-WUuo8nw',
-  authDomain: 'numeroconnect-637lh.firebaseapp.com',
-  measurementId: '',
-  messagingSenderId: '818339710146',
+  "projectId": "numeroconnect-637lh",
+  "appId": "1:818339710146:web:01271839b424e1d7d9675b",
+  "storageBucket": "numeroconnect-637lh.firebasestorage.app",
+  "apiKey": "AIzaSyDegTkPCfQC6nYziFLncbfnM7r-WUuo8nw",
+  "authDomain": "numeroconnect-637lh.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "818339710146"
 };
 
 // Initialize Firebase
